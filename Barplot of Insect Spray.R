af16519960b0ac4ded3c26004f6444c7bf80a18e
Ankur Maiti <33312337+ankurmaiti@users.noreply.github.com>
2017-11-04 22:27:01 -0700

    Update Barplot of Insect Spray.R

--- Barplot of Insect Spray.R
+++ Barplot of Insect Spray.R
@@ -4,4 +4,4 @@ sprays
 means <- aggregate(sprays$count ~ sprays$spray , FUN = mean)
 tranmeans.data <- t(means[-1])
 colnames(tranmeans.data) <- means[,1]
-barplot(tranmeans.data, ylim = c(0,20), main = "Mean Insect Count by Spray", xlab = "Type of Spray", ylab ="Mean number of Insects dead")
+barplot(tranmeans.data, ylim = c(0,20), col = rgb(20,150,200, max = 255),main = "Mean Insect Count by Spray", xlab = "Type of Spray", ylab ="Mean number of Insects dead")
